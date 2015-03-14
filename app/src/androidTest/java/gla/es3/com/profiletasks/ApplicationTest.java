0a682fd176bfb8d6b0f7e2818ec967d8566829cc
fernandosci <fernandosci@gmail.com>
2015-03-14 01:08:52 +0000

    initial commit  1

---
+++ app/src/androidTest/java/gla/es3/com/profiletasks/ApplicationTest.java
@@ -0,0 +1,13 @@
+package gla.es3.com.profiletasks;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
